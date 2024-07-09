3a9161fcc5d3d5156f78bea6d5a11c8f933c5ac5
KUSUMANJALI <KUSUMANJALI@DESKTOP-K92U4B5.Dlink>
2024-07-09 12:29:02 +0530

    commit

---
+++ corejavaprograms/src/com/tnsif/inheritance/Supermain.java
@@ -0,0 +1,14 @@
+package com.tnsif.inheritance;
+
+public class Supermain {
+	public static void main(String[] args) {
+		Student s=new Student("kusuma",1234567890,"hyd",993243107);
+		System.out.println(s);
+		
+		Student s1=new Student(1,"sriindhu");
+		System.out.println(s1);
+	}
+
+
+
+}
---
+++ corejavaprograms/src/com/tnsif/superkeyword/Superex.java
@@ -0,0 +1,16 @@
+package com.tnsif.superkeyword;
+// parent class
+	public class Superex {
+		
+		int a=20;
+		
+		void drinking() {
+			System.out.println("coffee");
+		}
+		Superex(){
+			System.out.println("hello");
+		}
+
+
+
+}
